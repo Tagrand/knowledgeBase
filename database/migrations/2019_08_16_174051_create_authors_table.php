2b6401b21403a1c81f348206e18bce3df780082b
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-08-16 21:02:11 +0100

    🎨  code formatting

--- database/migrations/2019_08_16_174051_create_authors_table.php
+++ database/migrations/2019_08_16_174051_create_authors_table.php
@@ -17,7 +17,6 @@ class CreateAuthorsTable extends Migration
             $table->bigIncrements('id');
             $table->string('first_name');
             $table->string('last_name');
-            
             $table->timestamps();
         });
     }
