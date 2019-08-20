18c301a86b9777d79034ae6c3939ff4d18d0a292
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-08-20 17:02:50 +0100

    ✅ facts cannot be made for sources that dont exist

--- tests/Api/SourcesFactsTest.php
+++ tests/Api/SourcesFactsTest.php
@@ -13,7 +13,6 @@ class SourcesFactsTest extends TestCase
     use RefreshDatabase;
 
     public function test_user_can_make_facts() {
-        $this->withoutExceptionHandling();
         $user = factory(User::class)->create();
         $source = factory(Source::class)->create();
 
@@ -27,5 +26,26 @@ class SourcesFactsTest extends TestCase
              'source_id' => $source->id, 
              'claim' => 'This is a claim',
          ]);
+    }
+
+    public function test_guests_cannot_make_facts() {
+        $source = factory(Source::class)->create();
+
+        $response = $this->json('POST', "/api/v1/sources/{$source->id}/facts", [
+            'claim' => 'This is a claim',
+        ]);
+
+        $response->assertStatus(401);
+    }
+
+    public function test_the_fact_must_have_a_source() {
+        $user = factory(User::class)->create();
+
+        Passport::actingAs($user);
+        $response = $this->json('POST', "/api/v1/sources/1234/facts", [
+            'claim' => 'This is a claim',
+        ]);
+
+         $response->assertStatus(404);
     }
 }
