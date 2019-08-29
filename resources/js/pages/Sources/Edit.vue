7d3845f32ae3f669586539c2acd5b362e630a42c
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-08-29 16:19:19 +0100

    🎨  format edit page

--- resources/js/pages/Sources/Edit.vue
+++ resources/js/pages/Sources/Edit.vue
@@ -1,63 +1,83 @@
 <template>
   <div>
     <div class="flex justify-between">
-      <h1 class="text-xl font-bold">Source</h1>
-      <h1 class="text-xl font-bold">Author</h1>
+      <h1 class="text-xl font-bold">
+        Source
+      </h1>
+      <h1 class="text-xl font-bold">
+        Author
+      </h1>
     </div>
 
     <div class="flex justify-between mb-4">
       <div>
         <div>
           <span>{{ selectedSource.name }}</span>
-          <span class="mr-4" v-show="!!selectedSource.summary">({{ selectedSource.summary }})</span>
+          <span
+            v-show="!!selectedSource.summary"
+            class="mr-4"
+          >({{ selectedSource.summary }})</span>
         </div>
-        <router-link class="hover:text-blue-400" to="/sources">Reset</router-link>
+        <router-link
+          class="hover:text-blue-400"
+          to="/sources"
+        >
+          Reset
+        </router-link>
       </div>
 
-      <author-picker-vue :source="selectedSource"></author-picker-vue>
+      <author-picker-vue :source="selectedSource" />
     </div>
 
-    <h1 class="text-xl font-bold">Fact</h1>
+    <h1 class="text-xl font-bold">
+      Fact
+    </h1>
 
     <search-vue
-      data-type="fact"
-      searchKey="claim"
-      @fact-save="saveFact"
       v-show="!isFactSelected"
+      data-type="fact"
+      search-key="claim"
       :collection="sourceFacts"
-      @fact-select="setSelectedFact"
       style="max-height: 200px; overflow-y: auto;"
-    ></search-vue>
+      @fact-save="saveFact"
+      @fact-select="setSelectedFact"
+    />
 
     <div v-show="isFactSelected">
       <div class="flex mb-4">
-        <p class="mr-4">{{ selectedFact.claim }}</p>
-        <button @click="setSelectedFact({})">Reset</button>
+        <p class="mr-4">
+          {{ selectedFact.claim }}
+        </p>
+        <button @click="setSelectedFact({})">
+          Reset
+        </button>
       </div>
 
-      <issue-picker-vue :fact="selectedFact"></issue-picker-vue>
+      <issue-picker-vue :fact="selectedFact" />
     </div>
   </div>
 </template>
 
 <script>
-import SearchVue from "../../components/Search.vue";
-import IssuePickerVue from "../../components/IssuePicker.vue";
-import AuthorPickerVue from "../../components/AuthorPicker.vue";
+import axios from 'axios';
+import SearchVue from '../../components/Search.vue';
+import IssuePickerVue from '../../components/IssuePicker.vue';
+import AuthorPickerVue from '../../components/AuthorPicker.vue';
 
 export default {
   components: { SearchVue, IssuePickerVue, AuthorPickerVue },
 
   props: {
     id: {
-      required: true
-    }
+      required: true,
+      type: String,
+    },
   },
 
   data() {
     return {
       sourceFacts: [],
-      selectedFact: {}
+      selectedFact: {},
     };
   },
 
@@ -68,16 +88,16 @@ export default {
 
     selectedSource() {
       return this.$store.state.selectedSource;
-    }
+    },
   },
 
   created() {
-    this.$store.dispatch("setSelectedSource", this.id);
+    this.$store.dispatch('setSelectedSource', this.id);
 
     axios
       .get(`/api/v1/sources/${this.id}/facts`)
       .then(({ data }) => (this.sourceFacts = data))
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   },
 
   methods: {
@@ -92,7 +112,7 @@ export default {
           this.sourceFacts.push(data);
           this.selectedFact = data;
         });
-    }
-  }
+    },
+  },
 };
 </script>
