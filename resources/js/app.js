94f395cf48e80cd0467eab64dc1ca9be7e47b2fe
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-08-29 16:14:53 +0100

    ✨  dont import axios globally

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,23 +4,22 @@ import Routes from '@/js/routes.js';
 import store from '@/js/store.js';
 
 window._ = require('lodash');
-window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-let token = document.head.querySelector('meta[name="csrf-token"]');
+const token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+  window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+  console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
 const app = new Vue({
-    el: '#app',
-    store,
-    router: Routes,
-    render: h => h(App),
+  el: '#app',
+  store,
+  router: Routes,
+  render: (h) => h(App),
 });
 
 export default app;
