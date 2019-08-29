18d4319e18bf2dbff04d1faa11de693506fb2da5
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-08-29 21:16:33 +0100

    🎨  format app

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -2,11 +2,26 @@
   <div>
     <div class="px-4">
       <div class="flex pb-4 justify-between pt-2 text-md">
-        <router-link class="hover:text-blue-400" to="/">Home</router-link>
-        <router-link class="hover:text-blue-400" to="/sources">Sources</router-link>
-        <router-link class="hover:text-blue-400" to="/issues">Issues</router-link>
+        <router-link
+          class="hover:text-blue-400"
+          to="/"
+        >
+          Home
+        </router-link>
+        <router-link
+          class="hover:text-blue-400"
+          to="/sources"
+        >
+          Sources
+        </router-link>
+        <router-link
+          class="hover:text-blue-400"
+          to="/issues"
+        >
+          Issues
+        </router-link>
       </div>
-      <router-view></router-view>
+      <router-view />
     </div>
   </div>
 </template>
