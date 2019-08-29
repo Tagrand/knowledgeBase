293daed6b9be9ad518306add70b5d8d399b05fa4
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-08-29 16:18:31 +0100

    🎨  format view page

--- resources/js/pages/Issues/View.vue
+++ resources/js/pages/Issues/View.vue
@@ -3,42 +3,49 @@
     <h1>{{ issue.name }}</h1>
     <h2>{{ issue.summary }}</h2>
 
-    <router-link :to="`/issues/${issue.id}/edit`">Edit</router-link>
+    <router-link :to="`/issues/${issue.id}/edit`">
+      Edit
+    </router-link>
 
-    <h2 class="pt-4">Facts</h2>
-    <div :key="fact.claim" v-for="fact in facts">
+    <h2 class="pt-4">
+      Facts
+    </h2>
+    <div
+      v-for="fact in facts"
+      :key="fact.claim"
+    >
       <span>{{ fact.claim }}</span>
       <span>({{ fact.source.name }})</span>
     </div>
 
-    <h2 class="pt-4">Related issues</h2>
-    <div :key="issue.name" v-for="issue in relatedIssues">
-      <router-link :to="`/issues/${issue.id}`">{{ issue.name }}</router-link>
+    <h2 class="pt-4">
+      Related issues
+    </h2>
+    <div
+      v-for="issue in relatedIssues"
+      :key="issue.name"
+    >
+      <router-link :to="`/issues/${issue.id}`">
+        {{ issue.name }}
+      </router-link>
     </div>
   </div>
 </template>
 <script>
+import axios from 'axios';
+
 export default {
   props: {
     id: {
-      required: true
-    }
+      required: true,
+      type: String,
+    },
   },
 
   data() {
     return {
-      facts: []
+      facts: [],
     };
-  },
-
-  created() {
-    this.resetIssue();
-  },
-
-  watch: {
-    id() {
-      this.resetIssue();
-    }
   },
 
   computed: {
@@ -48,28 +55,38 @@ export default {
 
     relatedIssues() {
       const related = [];
-      this.facts.forEach(fact => {
-        fact.issues.forEach(issue => {
+      this.facts.forEach((fact) => {
+        fact.issues.forEach((issue) => {
           if (
-            !_.some(related, option => option.id === issue.id) &&
-            issue.id != this.id
+            !_.some(related, (option) => option.id === issue.id)
+            && issue.id != this.id
           ) {
             related.push(issue);
           }
         });
       });
       return related;
-    }
+    },
+  },
+
+  watch: {
+    id() {
+      this.resetIssue();
+    },
+  },
+
+  created() {
+    this.resetIssue();
   },
 
   methods: {
     resetIssue() {
-      this.$store.dispatch("setSelectedIssue", this.id);
+      this.$store.dispatch('setSelectedIssue', this.id);
 
       axios
         .get(`/api/v1/issues/${this.id}/facts`)
         .then(({ data }) => (this.facts = data));
-    }
-  }
+    },
+  },
 };
 </script>
