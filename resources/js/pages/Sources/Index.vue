bafd64130b6bcf09e7a27967486d633f6d3f34c8
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-08-29 16:16:44 +0100

    🎨  format index pag

--- resources/js/pages/Sources/Index.vue
+++ resources/js/pages/Sources/Index.vue
@@ -7,40 +7,41 @@
       :collection="sources"
       @source-save="saveSource"
       @source-select="setSelectedSource"
-    ></search-vue>
+    />
   </div>
 </template>
 <script>
-import SearchVue from "../../components/Search.vue";
+import axios from 'axios';
+import SearchVue from '../../components/Search.vue';
 
 export default {
   components: { SearchVue },
 
-  created() {
-    this.$store.dispatch("getSources");
-  },
-
   computed: {
     sources() {
       return this.$store.state.sources;
-    }
+    },
+  },
+
+  created() {
+    this.$store.dispatch('getSources');
   },
 
   methods: {
     saveSource(info) {
-      let name, summary;
-      console.log(info);
-      [name, summary] = info.split("**");
+      let name; let summary;
 
-      axios.post("/api/v1/sources", { name, summary }).then(({ data }) => {
-        this.$store.commit("addSource", data);
-        this.$router.push({ name: "source.edit", params: { id: data.id } });
+      [name, summary] = info.split('**');
+
+      axios.post('/api/v1/sources', { name, summary }).then(({ data }) => {
+        this.$store.commit('addSource', data);
+        this.$router.push({ name: 'source.edit', params: { id: data.id } });
       });
     },
 
     setSelectedSource(source) {
-      this.$router.push({ name: "source.edit", params: { id: source.id } });
-    }
-  }
+      this.$router.push({ name: 'source.edit', params: { id: source.id } });
+    },
+  },
 };
 </script>
