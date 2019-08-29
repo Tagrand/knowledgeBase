2297271ee762aace0746a3544b08801bb3833d77
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-08-29 21:23:07 +0100

    🎨  format Search

--- resources/js/components/Search.vue
+++ resources/js/components/Search.vue
@@ -1,86 +1,100 @@
 <template>
   <div>
-    <input type="text" :placeholder="dataType" v-model="search" />
-    <button @click="saveOption" v-show="isNewOption">Save as new</button>
+    <input
+      v-model="search"
+      type="text"
+      :placeholder="dataType"
+    >
+    <button
+      v-show="isNewOption"
+      @click="saveOption"
+    >
+      Save as new
+    </button>
 
-    <div class="overflow-y-auto" style="max-height: 100px">
+    <div
+      class="overflow-y-auto"
+      style="max-height: 100px"
+    >
       <div
-        @click="selectOption(option)"
-        v-bind:key="option[searchKey]"
         v-for="option in filteredCollection"
+        :key="option[searchKey]"
+        @click="selectOption(option)"
       >
-        <span v-text="option[searchKey]"></span>
-        <span v-show="!!option[extraInfo]" v-text="`(${option[extraInfo]})`"></span>
+        <span v-text="option[searchKey]" />
+        <span
+          v-show="!!option[extraInfo]"
+          v-text="`(${option[extraInfo]})`"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
   props: {
     collection: {
       type: Array,
-      required: true
+      required: true,
     },
 
     searchKey: {
       type: String,
-      default: "name"
+      default: 'name',
     },
 
     dataType: {
       type: String,
-      required: true
+      required: true,
     },
 
     extraInfo: {
-      type: String
-    }
+      type: String,
+      default: null,
+    },
   },
 
   data() {
     return {
-      search: ""
+      search: '',
     };
   },
 
   computed: {
     filteredCollection() {
-      if (this.search === "") {
+      if (this.search === '') {
         return this.collection;
       }
 
-      return _.filter(this.collection, option => {
-        return option[this.searchKey]
-          .toLowerCase()
-          .includes(this.search.toLowerCase());
-      });
+      return _.filter(this.collection, (option) => option[this.searchKey]
+        .toLowerCase()
+        .includes(this.search.toLowerCase()));
     },
 
     isNewOption() {
-      if (this.search === "") {
+      if (this.search === '') {
         return false;
       }
 
-      return !_.some(this.collection, option => {
-        return (
-          option[this.searchKey].toLowerCase() === this.search.toLowerCase()
-        );
-      });
-    }
+      return !_.some(this.collection, (option) => (
+        option[this.searchKey].toLowerCase() === this.search.toLowerCase()
+      ));
+    },
   },
 
   methods: {
     selectOption(option) {
       this.$emit(`${this.dataType}-select`, option);
-      this.search = "";
+      this.search = '';
     },
 
     saveOption() {
       this.$emit(`${this.dataType}-save`, this.search);
-      this.search = "";
-    }
-  }
+      this.search = '';
+    },
+  },
 };
 </script>
