5efc5d2e550fb2c88115618a8ca84d4cdf984617
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-08-30 19:25:10 +0100

    ♻  make route pages more clear

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -7,9 +7,8 @@ import SourcesIndex from '@/js/pages/Sources/Index';
 import SourcesView from '@/js/pages/Sources/View';
 import SourcesEdit from '@/js/pages/Sources/Edit';
 import IssuesIndex from '@/js/pages/Issues/Index';
-import View from '@/js/pages/Issues/View';
+import IssuesView from '@/js/pages/Issues/View';
 import IssuesEdit from '@/js/pages/Issues/Edit';
-
 
 Vue.use(VueRouter);
 
@@ -46,7 +45,7 @@ const router = new VueRouter({
     {
       path: '/issues/:id',
       name: 'issues.view',
-      component: View,
+      component: IssuesView,
       props: true,
     },
     {
