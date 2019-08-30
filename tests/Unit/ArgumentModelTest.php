da805ce87da525635ffb118def04391638c635d4
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-08-30 17:47:22 +0100

    ✅  test argument model

---
+++ tests/Unit/ArgumentModelTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Argument;
+use App\Source;
+use Tests\TestCase;
+use Illuminate\Database\QueryException;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ArgumentModelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_you_can_make_an_argument()
+    {
+        $argument = new Argument([
+            'reason' => 'Science',
+        ]);
+
+        $this->assertTrue($argument->save());
+        $this->assertDatabaseHas('arguments', [
+            'reason' => 'Science',
+        ]);
+    }
+
+    public function test_arguments_can_be_linked_to_a_source()
+    {
+        $source = factory(Source::class)->create();
+        $argument = new Argument([
+            'reason' => 'Faith',
+            'source_id' => $source->id,
+        ]);
+
+        $this->assertTrue($argument->save());
+        $this->assertDatabaseHas('arguments', [
+            'reason' => 'Faith',
+            'source_id' => $source->id,
+        ]);
+    }
+
+    public function test_reasons_are_required()
+    {
+        $this->expectException(QueryException::class);
+        $argument = new Argument();
+
+        $this->assertFalse($argument->save());
+    }
+
+}
