74de3c8ba1408e3eff7e5ba55be95e0babfd7ff6
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-09-01 20:18:21 +0100

    🎨  order apis alphabetically

--- routes/api.php
+++ routes/api.php
@@ -22,25 +22,28 @@ Route::prefix('v1')->group(function () {
         Route::get('/authors', 'AuthorsController@index');
         Route::post('/authors', 'AuthorsController@store');
 
+        Route::get('/facts', 'FactsController@index');
+        Route::patch('/facts/{fact}', 'FactsController@update');
+
+        Route::get('/facts/{fact}/issues', 'FactsIssuesController@index');
+        Route::post('/facts/{fact}/issues/{issue}', 'FactsIssuesController@store');
+        Route::delete('/facts/{fact}/issues/{issue}', 'FactsIssuesController@destroy');
+
+        Route::get('/issues', 'IssuesController@index');
+        Route::post('/issues', 'IssuesController@store');
+        Route::patch('/issues/{issue}', 'IssuesController@update');
+
+        Route::get('/issues/{issue}/facts', 'IssuesFactsController@index');
+
         Route::get('/sources', 'SourcesController@index');
         Route::post('/sources', 'SourcesController@store');
         Route::patch('/sources/{source}', 'SourcesController@update');
 
         Route::get('/sources/{source}/facts', 'SourcesFactsController@index');
         Route::post('/sources/{source}/facts', 'SourcesFactsController@store');
+
         Route::get('/sources/{source}/authors', 'SourcesAuthorsController@index');
         Route::post('/sources/{source}/authors/{author}', 'SourcesAuthorsController@store');
         Route::delete('/sources/{source}/authors/{author}', 'SourcesAuthorsController@destroy');
-
-        Route::get('/issues', 'IssuesController@index');
-        Route::post('/issues', 'IssuesController@store');
-        Route::patch('/issues/{issue}', 'IssuesController@update');
-        Route::get('/issues/{issue}/facts', 'IssuesFactsController@index');
-
-        Route::get('/facts', 'FactsController@index');
-        Route::patch('/facts/{fact}', 'FactsController@update');
-        Route::get('/facts/{fact}/issues', 'FactsIssuesController@index');
-        Route::post('/facts/{fact}/issues/{issue}', 'FactsIssuesController@store');
-        Route::delete('/facts/{fact}/issues/{issue}', 'FactsIssuesController@destroy');
     });
 });
