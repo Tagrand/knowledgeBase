5e7b1daffc9162b9bdaf0f97540f3c813e1ed117
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-09-05 21:28:43 +0100

    ✅  summary can be null

--- tests/Feature/Api/ArgumentsTest.php
+++ tests/Feature/Api/ArgumentsTest.php
@@ -215,4 +215,22 @@ class ArgumentsTest extends TestCase
         $response->assertStatus(422);
         $response->assertJsonValidationErrors('reason');
     }
+
+    public function test_summaries_can_be_null()
+    {
+        $user = factory(User::class)->create();
+        $source = factory(Source::class)->create();
+        $argument = factory(Argument::class)->create();
+
+        Passport::actingAs($user);
+        $response = $this->json('PATCH', "/api/v1/arguments/{$argument->id}", [
+            'summary' => '',
+        ]);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('arguments', [
+             'id' => $argument->id,
+            'summary' => null,
+        ]);
+    }
 }
