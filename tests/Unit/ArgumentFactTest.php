bad942e8e8f0497c1374f9469c0418747e219bfe
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-09-06 14:10:50 +0100

    ✅  arguments and facts are joined as true by default

--- tests/Unit/ArgumentFactTest.php
+++ tests/Unit/ArgumentFactTest.php
@@ -12,16 +12,16 @@ class ArgumentFactTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_you_can_join_arguments_and_issues()
+    public function test_you_can_join_arguments_and_facts()
     {
         $argument = factory(Argument::class)->create();
         $fact = factory(Fact::class)->create();
-        $argument->facts()->attach($fact, ['is_supportive' => true]);
+        $argument->facts()->attach($fact, ['is_supportive' => false]);
 
         $this->assertDatabaseHas('argument_fact', [
             'argument_id' => $argument->id,
             'fact_id' => $fact->id,
-            'is_supportive' => true,
+            'is_supportive' => false,
         ]);
     }
 
@@ -35,5 +35,18 @@ class ArgumentFactTest extends TestCase
         $argument->facts()->attach($fact);
 
         $this->assertTrue(true);
+    }
+
+    public function test_arguments_are_attached_as_supportive_by_default()
+    {
+        $argument = factory(Argument::class)->create();
+        $fact = factory(Fact::class)->create();
+        $argument->facts()->attach($fact);
+
+        $this->assertDatabaseHas('argument_fact', [
+            'argument_id' => $argument->id,
+            'fact_id' => $fact->id,
+            'is_supportive' => true,
+        ]);
     }
 }
