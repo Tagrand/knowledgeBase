6153628ca9f279557a6190f099bbf3eb6ecce0cd
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-10-06 15:17:00 +0100

    ✅  images must be a string

--- tests/Feature/Api/SourcesFactsTest.php
+++ tests/Feature/Api/SourcesFactsTest.php
@@ -145,4 +145,19 @@ class SourcesFactsTest extends TestCase
         $response->assertStatus(422);
         $response->assertJsonValidationErrors('summary');
     }
+
+    public function test_a_image_must_be_a_string()
+    {
+        $user = factory(User::class)->create();
+        $source = factory(Source::class)->create();
+
+        Passport::actingAs($user);
+        $response = $this->json('POST', "/api/v1/sources/{$source->id}/facts", [
+            'claim' => 'asdasd',
+            'image' => 121212,
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('image');
+    }
 }
